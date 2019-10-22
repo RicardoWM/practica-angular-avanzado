5d836b48358deac0bae9a43a6d1943708256b6f3
Ricardo Lobo <wolfmoon.developer@outlook.com>
2019-10-22 16:47:50 +0200

    'pub'

--- apps/banco-mundial/src/app/app.module.ts
+++ apps/banco-mundial/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { LayoutModule } from '../../../../libs/shared/layout/src/lib/layout.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- libs/shared/layout/src/lib/layout.module.ts
+++ libs/shared/layout/src/lib/layout.module.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { LayoutComponent } from './layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [HeaderComponent, MainComponent, FooterComponent, LayoutComponent],
   exports: [LayoutComponent]
